e1f453c1184e1d55c369938c03a2b919c38a91e6
Nelto <32131337+Nelto@users.noreply.github.com>
2018-10-29 19:09:11 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Verification
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0 || args[0].Equals("?")) Console.WriteLine("Введите имя входного файла");
+            else
+            {
+                Verific verific = new Verific(args[0]);
+                Console.WriteLine("Кол-во сгенерированных символов:");
+                verific.PrintCountingRes();
+                Console.WriteLine("\nУсловные вероятности:");
+                verific.PrintChances();
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Verific.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Verification
+{
+    class Verific
+    {
+        int[] count = new int[256];
+        string chars;
+
+      public  Verific(String input)
+        {
+            try
+            {
+                using (StreamReader sr = new StreamReader("Res.txt", System.Text.Encoding.Default))
+                {
+                    chars = sr.ReadToEnd();
+                }
+                Count();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        private void Count()
+        {
+            for (int i = 0; i < chars.Length; i++)
+            {
+                count[chars[i]]++;
+            }
+        }
+
+        public void PrintCountingRes()
+        {
+            for (int i = 0; i < count.Length; i++)
+            {
+                if (count[i] > 0) Console.Write((char)i + "  " + count[i] + "\n");
+            }
+        }
+
+        public void PrintChances()
+        {
+            for (int i = 0; i < count.Length; i++)
+            {
+                if (count[i] > 0) Console.Write("{0}  {1:0.#####}\n", (char) i, (double)count[i] / chars.Length);
+            }
+        }
+    }
+}
